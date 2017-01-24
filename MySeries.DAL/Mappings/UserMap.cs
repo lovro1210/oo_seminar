acc1e23c9b0d97496f39f4ca0dc7b7e1ac53a942
fstivicic <f.stivicic@gmail.com>
2017-01-24 13:36:55 +0100

    Pokusaj schema export

---
+++ MySeries.DAL/Mappings/UserMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using MySeries.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeries.DAL.Mappings
+{
+    public class UserMap : ClassMap<User>
+    {
+            public UserMap()
+            {
+                Id(b => b.id).GeneratedBy.Native();
+                Map(b => b.name);
+                Map(b => b.surname);
+                Map(b => b.sex);
+                Map(b => b.email);
+                Map(b => b.password);
+            }
+    }
+}
---
+++ MySeries.Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeries.Model
+{
+    public class User
+    {
+        public virtual int id { get; set; }
+        public virtual String name { get; set; }
+        public virtual String surname { get; set; }
+        public virtual Boolean sex { get; set; }
+        public virtual String email { get; set; }
+        public virtual String password { get; set; }
+    }
+}
